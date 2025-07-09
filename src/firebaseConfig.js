2e88c37ecafda93246e068ba80639c1833838362
kamekingdom <yudainakamura2002@gmail.com>
2025-07-10 05:13:06 +0900

    Update

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAGSzbervysYJ9_70W2ro10YxxNAP8NLCQ",
+  authDomain: "kame-puyopuyo.firebaseapp.com",
+  projectId: "kame-puyopuyo",
+  storageBucket: "kame-puyopuyo.firebasestorage.app",
+  messagingSenderId: "355041562973",
+  appId: "1:355041562973:web:3e99ee08ae3d2bea8b98b4",
+  measurementId: "G-0T93KQPW3B"
 };
